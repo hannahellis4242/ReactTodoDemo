d16d65a6b3e410142cc2395dd84040cb7f430558
Hannah Ellis <hannahellis4242@gmail.com>
2022-02-08 12:58:12 +0000

    Styling the form a little bit and adding some functionalit to disable the submit button untill user adds some sort of input value

--- my-app/src/Components/AddTodo.tsx
+++ my-app/src/Components/AddTodo.tsx
@@ -1,23 +1,48 @@
+import classes from "./AddTodo.module.css";
 import React, { useContext, useRef } from "react";
 import { TodoContext } from "../store/TodoContext";
 
 const AddTodo: React.FC = () => {
   const context = useContext(TodoContext);
   const inputRef = useRef<HTMLInputElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   const onSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     const input = inputRef.current;
-    if (input) {
+    if (input && input.value !== "") {
       context.addItem(input.value);
+      input.value = "";
+      handleInputChanged();
+    }
+  };
+
+  const handleInputChanged = () => {
+    const button = buttonRef.current;
+    if (button) {
+      const input = inputRef.current;
+      if (input) {
+        button.disabled = input.value === "";
+      }
     }
   };
 
   return (
-    <form onSubmit={onSubmitHandler}>
-      <label htmlFor="text"></label>
-      <input type="text" id="text" ref={inputRef}></input>
-      <button>Add</button>
+    <form className={classes.add} onSubmit={onSubmitHandler}>
+      <div className={classes.input}>
+        <label htmlFor="text">What do you need to do? </label>
+        <input
+          type="text"
+          id="text"
+          ref={inputRef}
+          onChange={handleInputChanged}
+        ></input>
+      </div>
+      <div className={classes.submit}>
+        <button ref={buttonRef} disabled={true}>
+          Add
+        </button>
+      </div>
     </form>
   );
 };
